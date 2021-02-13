4c08a53b10631228d3f22c94085fb3430df703b6
Christian Schwer <christianschwer00@gmail.com>
2021-02-13 10:38:07 +0100

    Added background picture

--- ScreenSaver.py
+++ ScreenSaver.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter.ttk import *
 from time import *
 from datetime import *
+from PIL import Image
+from PIL import ImageTk
 
 class Fullscreen:
     def __init__(self):
@@ -14,17 +16,29 @@ class Fullscreen:
         self.window.bind("<Button-3>", self.quitFullScreen)
         self.window.bind("<Motion>", self.quitFullScreen)
 
-        timer_lbl = Label(self.window, font = ('calibri', 30, 'bold'), foreground = 'black')
-        time_lbl = Label(self.window, font = ('calibri', 40, 'bold'), foreground = 'black')
+        #Get screen resolutions
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
 
-        timer_lbl.place(relx = 0.5, rely = 0.5, anchor = 'center')
-        time_lbl.place(relx = 0.5, rely = 0.1, anchor = 'center')
+        #Add image file
+        #filename = "D:\Privat\Bilder\Lonely-Boat-Dragon-Bones-LIve-Wallpaper.gif"
+        filename = "D:\Privat\Bilder\IMG_3750.JPG"
+        background_image = Image.open(filename)
+        background_image = background_image.resize((screen_width, screen_height), Image.ANTIALIAS)
+        background_image = ImageTk.PhotoImage(background_image)
+
+        #Create Canvas
+        canvas = Canvas(self.window, width = 1020, height = 1080)
+        canvas.pack(fill = "both", expand = True)
+
+        canvas.create_image(0, 0, image = background_image, anchor = "nw")
+        timerObj = canvas.create_text((screen_width/2, screen_height/2), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
+        timeObj = canvas.create_text((screen_width/2, screen_height/4), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
 
         end_Date = datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)
 
-        Countdown(timer_lbl, end_Date)
-        time(time_lbl)
-
+        Countdown(canvas, end_Date, timerObj)
+        time(canvas, timeObj)
         self.window.mainloop()
 
     def quitFullScreen(self, event):
@@ -32,7 +46,7 @@ class Fullscreen:
         self.window.attributes("-fullscreen", self.fullScreenState)    #   only minimize
         #self.window.destroy()      #-  close window
 
-def Countdown(lbl1, end_Date):
+def Countdown(canvas, end_Date, timerObj):
     now = datetime.now()
     years = end_Date.year - now.year - ((end_Date.month, end_Date.day) < (now.month, now.day))
     months =  11 - (now.month - end_Date.month)
@@ -40,17 +54,16 @@ def Countdown(lbl1, end_Date):
     hours = 23 - now.hour - end_Date.hour
     minutes = 59 - now.minute - end_Date.minute
     seconds = 60 - now.second - end_Date.second
+    text = 'Years ' + str(years) + '    Months ' + str(months) + '    Days ' + str(days) +\
+        '    Hours ' + str(hours) + '    Minutes ' + str(minutes) + '    Seconds ' + str(seconds)
     if int(end_Date.strftime('%Y%m%d%H%M%S')) > 0:
-        lbl1.config(text = 'Years ' + str(years) +
-        '    Months ' + str(months) + '    Days ' + str(days) +
-        '    Hours ' + str(hours) + '    Minutes ' + str(minutes) +
-        '    Seconds ' + str(seconds))
-        lbl1.after(1000, Countdown, lbl1, end_Date)
+        canvas.itemconfigure(timerObj, text = text)
+        canvas.after(1000, Countdown, canvas, end_Date, timerObj)
 
-def time(lbl):
+def time(canvas, timeObj):
     string = strftime('%H:%M:%S')
-    lbl.config(text = string)
-    lbl.after(1000, time, lbl)
+    canvas.itemconfigure(timeObj, text = string)
+    canvas.after(1000, time, canvas, timeObj)
 
 if __name__ == '__main__':
     app = Fullscreen()

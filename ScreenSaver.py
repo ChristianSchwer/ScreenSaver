8b512bbcc1e8185628747600d7f14719d08791dd
Christian Schwer <christianschwer00@gmail.com>
2021-03-01 11:17:29 +0100

    Added GUI

--- ScreenSaver.py
+++ ScreenSaver.py
@@ -10,77 +10,150 @@ import mouse
 
 last_input = datetime.now()
 lastPosition = mouse.get_position()
-'''
-d = display.Display()
-s = d.screen()
-xwindow = s.root.create_window(0,0,1,1,1,s.root_depth)
 
-res = randr.get_screen_resources(xwindow)
-for mode in res.modes:
-    w, h = mode.width, mode.height
-    print("width: {}, height: {}".format(w,h))
-'''
-class Fullscreen:
+class GUI:
     def __init__(self):
-        self.window = Tk()
-        self.window.attributes('-fullscreen', True)
-        UserInput(self)
+        gui = Tk()
+        gui.geometry("600x400")
 
-        #Get screen resolutions
-        screen_width = self.window.winfo_screenwidth()
-        screen_height = self.window.winfo_screenheight()
+        #GUI enteries
+        canvasG = Canvas(gui, width = 600, height = 400)
+        canvasG.pack(fill = "both", expand = True)
 
-        #Add image file
-        filename = "D:\Privat\Bilder\IMG_3750.JPG"
-        background_image = Image.open(filename)
-        background_image = background_image.resize((screen_width, screen_height), Image.ANTIALIAS)
-        background_image = ImageTk.PhotoImage(background_image)
+        #Enable picture
+        enableP = IntVar()
+        CheckP = Checkbutton(gui, text='Picture',variable=enableP, onvalue=1, offvalue=0)
+        canvasG.create_window(50, 25, window = CheckP)
 
-        #Add gif file
-        gif_filename = 'D:\\Privat\\Bilder\\Lonely-Boat-Dragon-Bones-LIve-Wallpaper.gif' #'D:\\Privat\\Bilder\\animated-wallpaper-windows-10.gif'
-        gif = Image.open(gif_filename)
-        #Add and count frames
-        frames = []
-        try:
-            while 1:
-                gif1 = gif.resize((screen_width, screen_height), Image.ANTIALIAS)
-                frames.append(ImageTk.PhotoImage(gif1.copy()))
-                gif.seek(len(frames))
-        except EOFError:
-            pass
-        frameCnt = len(frames)
+        labelP = Label(gui, text='Enter picture filename')
+        labelP.config(font=('helvetica', 14))
+        canvasG.create_window(300, 25, window = labelP)
 
-        #Create Canvas
-        canvas = Canvas(self.window, width = 1020, height = 1080)
-        canvas.pack(fill = "both", expand = True)
+        picturefile = Entry(gui)
+        canvasG.create_window(300, 50, window = picturefile)
 
-        #Create picture and text on canvas
-        canvas.create_image(0, 0, image = background_image, anchor = "nw")
-        animationObj = canvas.create_image(0, 0, image = frames[0], anchor = "nw")
-        timerObj = canvas.create_text((screen_width/2, screen_height/2), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
-        timeObj = canvas.create_text((screen_width/2, screen_height/4), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
+        #Enable animation
+        enableA = IntVar()
+        CheckA = Checkbutton(gui, text='Animation',variable=enableA, onvalue=1, offvalue=0)
+        canvasG.create_window(50, 100, window = CheckA)
 
-        #End date for the timer
-        end_Date = datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)
+        labelA = Label(gui, text='Enter animation filename')
+        labelA.config(font=('helvetica', 14))
+        canvasG.create_window(300, 100, window = labelA)
 
-        Update(canvas, 0, animationObj, frames, frameCnt)
-        Countdown(canvas, end_Date, timerObj)
-        Time(canvas, timeObj)
-        CreateWindow(self)
+        animationfile = Entry(gui)
+        canvasG.create_window(300, 125, window = animationfile)
 
-        self.window.mainloop
+        #Enable Time
+        enableT = IntVar()
+        CheckT = Checkbutton(gui, text='Time',variable=enableT, onvalue=1, offvalue=0)
+        canvasG.create_window(50, 175, window = CheckT)
 
-def CreateWindow(self):
-    t = Toplevel(self.window)
-    t.mainloop()
+        #Enable Countdown
+        enableC = IntVar()
+        CheckC = Checkbutton(gui, text='Countdown',variable=enableC, onvalue=1, offvalue=0)
+        canvasG.create_window(50, 225, window = CheckC)
 
-def QuitFullScreen(self):
-    self.window.withdraw()
+        labelD = Label(gui, text='Enter end date YYYY-MM-DD HH:MM:SS')
+        labelD.config(font=('helvetica', 14))
+        canvasG.create_window(300, 225, window = labelD)
+
+        endDateEntry = Entry(gui)
+        canvasG.create_window(300, 250, window = endDateEntry)
+
+        def CreateScreenSaver():
+            scs = Toplevel(gui)
+            scs.attributes('-fullscreen',True)
+
+            #Get screen resolutions
+            screen_width = scs.winfo_screenwidth()
+            screen_height = scs.winfo_screenheight()
+
+            #Create Canvas
+            canvas = Canvas(scs, width = 1020, height = 1080)
+            canvas.pack(fill = "both", expand = True)
+
+            picturefilename = picturefile.get()
+            animationfilename = animationfile.get()
+            #End date for the timer
+            endDate = endDateEntry.get()
+            if (endDate == ""):
+                end_Date = datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0)
+            else:
+                end_Date = datetime.strptime(endDate, '%Y-%m-%d %H:%M:%S')
+
+            if (enableP.get() == 1):
+                if (picturefilename != ""):
+                    #Add image file
+                    filename = picturefilename
+                    background_image = Image.open(filename)
+                    background_image = background_image.resize((screen_width, screen_height), Image.ANTIALIAS)
+                    background_image = ImageTk.PhotoImage(background_image)
+                    canvas.create_image(0, 0, image = background_image, anchor = "nw")
+                else:
+                    #Add image file
+                    filename = "D:\Privat\Bilder\IMG_3750.JPG"
+                    background_image = Image.open(filename)
+                    background_image = background_image.resize((screen_width, screen_height), Image.ANTIALIAS)
+                    background_image = ImageTk.PhotoImage(background_image)
+                    canvas.create_image(0, 0, image = background_image, anchor = "nw")
+
+            if (enableA.get() == 1):
+                if (animationfilename != ""):
+                    #Add gif file
+                    gif_filename = animationfilename
+                    gif = Image.open(gif_filename)
+                    #Add and count frames
+                    frames = []
+                    try:
+                        while 1:
+                            gif1 = gif.resize((screen_width, screen_height), Image.ANTIALIAS)
+                            frames.append(ImageTk.PhotoImage(gif1.copy()))
+                            gif.seek(len(frames))
+                    except EOFError:
+                        pass
+                    frameCnt = len(frames)
+                    animationObj = canvas.create_image(0, 0, image = frames[0], anchor = "nw")
+                    Animation(canvas, 0, animationObj, frames, frameCnt)
+                else:
+                    #Add gif file
+                    gif_filename = 'D:\\Privat\\Bilder\\Lonely-Boat-Dragon-Bones-LIve-Wallpaper.gif' #'D:\\Privat\\Bilder\\animated-wallpaper-windows-10.gif'
+                    gif = Image.open(gif_filename)
+                    #Add and count frames
+                    frames = []
+                    try:
+                        while 1:
+                            gif1 = gif.resize((screen_width, screen_height), Image.ANTIALIAS)
+                            frames.append(ImageTk.PhotoImage(gif1.copy()))
+                            gif.seek(len(frames))
+                    except EOFError:
+                        pass
+                    frameCnt = len(frames)
+                    animationObj = canvas.create_image(0, 0, image = frames[0], anchor = "nw")
+                    Animation(canvas, 0, animationObj, frames, frameCnt)
+
+            UserInput(scs)
+
+            if (enableC.get() == 1):
+                timerObj = canvas.create_text((screen_width/2, screen_height/2), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
+                Countdown(canvas, end_Date, timerObj)
+            if (enableT.get() == 1):
+                timeObj = canvas.create_text((screen_width/2, screen_height/4), font = ('calibri', 30, 'bold'), fill = 'white', text = '')
+                Time(canvas, timeObj)
+
+            scs.mainloop()
+
+        activateScreenSaver = Button(gui, text="erstellen", command = CreateScreenSaver)
+        canvasG.create_window(300, 380, window=activateScreenSaver)
+        gui.mainloop()
+
+def QuitFullScreen(scs):
+    scs.withdraw()
     global last_input
     last_input = datetime.now()
-    FullScreen(self)
+    FullScreen(scs)
 
-def UserInput(self):
+def UserInput(scs):
     global lastPosition
     currentPosition = mouse.get_position()
     if (keyboard.is_pressed("space") or keyboard.is_pressed("enter") or keyboard.is_pressed("q")
@@ -95,17 +168,17 @@ def UserInput(self):
      or keyboard.is_pressed("m") or keyboard.is_pressed("ü") or keyboard.is_pressed("ä")
      or keyboard.is_pressed("ö") or keyboard.is_pressed(",") or keyboard.is_pressed(".")
      or keyboard.is_pressed("-") or keyboard.is_pressed("#") or keyboard.is_pressed("+")):
-        QuitFullScreen(self)
+        QuitFullScreen(scs)
     if (mouse.is_pressed("right")):
-        QuitFullScreen(self)
+        QuitFullScreen(scs)
     if (mouse.is_pressed("middle")):
-        QuitFullScreen(self)
+        QuitFullScreen(scs)
     if (mouse.is_pressed("left")):
-        QuitFullScreen(self)
+        QuitFullScreen(scs)
     if (currentPosition != lastPosition):
         lastPosition = mouse.get_position()
-        QuitFullScreen(self)
-    self.window.after(100, UserInput, self)
+        QuitFullScreen(scs)
+    scs.after(100, UserInput, scs)
 
 def Countdown(canvas, end_Date, timerObj):
     now = datetime.now()
@@ -126,20 +199,20 @@ def Time(canvas, timeObj):
     canvas.itemconfigure(timeObj, text = string)
     canvas.after(1000, Time, canvas, timeObj)
 
-def Update(canvas, ind, gif, frames, frameCnt):
+def Animation(canvas, ind, gif, frames, frameCnt):
     frame = frames[ind]
     ind += 1
     if ind == frameCnt:
         ind = 0
     canvas.itemconfigure(gif, image=frame)
-    canvas.after(100, Update, canvas, ind, gif, frames, frameCnt)
+    canvas.after(100, Animation, canvas, ind, gif, frames, frameCnt)
 
-def FullScreen(self):
+def FullScreen(scs):
     current_time = datetime.now()
     inactivityTime = current_time - last_input
     if (inactivityTime.seconds > 5):
-        self.window.deiconify()
-    self.window.after(1000, FullScreen, self)
+        scs.deiconify()
+    scs.after(1000, FullScreen, scs)
 
 if __name__ == '__main__':
-    app = Fullscreen()
+    app = GUI()
